64a6a4e555d78fa51c996d497c5d677da5f6beab
Luke Dagnillo <lucianodagnillo@gmail.com>
2024-12-30 22:10:26 -0700

    Tested the LEDs and add NeoPixel and FastLED libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+// Definitions
+#define LED_PIN 5 
+#define NUM_LEDS 100
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  strip.begin();
+  strip.show(); // Initialize all LEDs to off
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Color wipe effect
+  for (int i = 0; i < strip.numPixels(); i++){
+    strip.setPixelColor(i, strip.Color(255, 0, 0)); // red color
+    strip.show();
+    delay(50);
+  }
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
